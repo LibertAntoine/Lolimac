ce95a2bcb4cc1fa6c4d47539be468e22da55565b
Nicolas LIENART <nicolas@lienart.net>
2019-04-07 22:21:39 +0200

    Removing private information from git
    You must create a file called dbID.php that contains your DB logins.
    Please refer to DBAccess for the variable names.

--- api/models/DBAccess.php
+++ api/models/DBAccess.php
@@ -4,13 +4,13 @@ namespace models;
 
 // Classe mère de tous les modèles, gérant l'identification d'accès à la base de donnée.
 abstract class DBAccess {
-	
+
   	protected $db;
 
 	public function __construct()
 	{
-		$db = new \PDO('mysql:host=localhost;dbname=lolimac', 'root', '');
+    include "dbID.php"; // This file must not be staged
+		$db = new \PDO("mysql:host={$dbHost};dbname={$dbTableName}", $dbUsername, $dbPassword);
 		$this->db = $db;
 	}
 }
-
