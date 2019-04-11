41ece9a31746af0161fe91dbc2b2ea80ae4bf9ff
JulesFouchy <45451201+JulesFouchy@users.noreply.github.com>
2019-04-11 18:23:26 +0200

    Add Place controller

---
+++ api/controllers/PlaceCRUD.php
@@ -0,0 +1,58 @@
+<?php
+
+	namespace controllers;
+
+	use \controllers\scans\ScanDataIn;
+    use \models\Place;
+    use \models\PlaceManager;
+
+
+class PlaceCRUD {
+
+	public function add($dataIn) {
+		$scanDataIn = new ScanDataIn();
+        $scanDataIn->exists($dataIn, ["postcode", "street", "number", "city", "name"]);
+        $data = $scanDataIn->failleXSS($dataIn);
+		$user = new Place($data);
+
+		$placeManager = new PlaceManager();
+		$placeManager->add($place);
+		return TRUE;
+	}
+
+	public function update($dataIn) {
+		$scanDataIn = new ScanDataIn();
+        $data = $scanDataIn->failleXSS($dataIn);
+		$placeManager = new PlaceManager();
+		$place = $placeManager->readById($data["id"]);
+		if($place) {
+			$place->hydrate($data);
+			$placeManager->update($place);
+		} else {
+			throw new Exception("Le lieu n'existe pas.");
+		}
+	}
+
+	public function read($dataIn) {
+		$scanDataIn = new ScanDataIn();
+        $scanDataIn->exists($dataIn, ["id"]);
+        $data = $scanDataIn->failleXSS($dataIn);
+		$placeManager = new PlaceManager();
+		$place = $placeManager->readById($data["id"]);
+		if($place) {
+			echo json_encode(array("name" => $place->GetName(), "postcode" => $place->GetPostcode(), "street" => $place->GetStreet(), "number" => $place->GetNumber(), "city" => $place->GetCity()));
+		} else {
+			throw new Exception("Le lieu n'existe pas.");
+		}
+
+	}
+
+	public function delete($dataIn) {
+		$scanDataIn = new ScanDataIn();
+        $scanDataIn->exists($dataIn, ["id"]);
+        $data = $scanDataIn->failleXSS($dataIn);
+		$placeManager = new PlaceManager();
+		$placeManager->deleteById($data["id"]);
+		return TRUE;
+	}
+}
