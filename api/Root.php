3bdbc5a87df3f23de425f545994752e38003188d
JulesFouchy <45451201+JulesFouchy@users.noreply.github.com>
2019-04-12 11:19:09 +0200

    Add Places to Root

--- api/Root.php
+++ api/Root.php
@@ -3,6 +3,7 @@
 // Fichier de routage de l'api.
 
     use \controllers\UserCRUD;
+    use \controllers\PlaceCRUD;
     use \controllers\scans\ScanDataIn; // prise en concidérations des namespaces.
 
 
@@ -36,6 +37,37 @@ class Root {
                 $_DELETE = json_decode(file_get_contents("php://input"), TRUE);
                 $userCRUD = new UserCRUD();
                 $userCRUD->delete($_DELETE);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public function place() {
+        // verif de token à ajouter.
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $placeCRUD = new PlaceCRUD();
+                $placeCRUD->read($_GET);
+                break;
+
+            case 'POST':
+                $_POST = json_decode(file_get_contents("php://input"), TRUE);
+                $placeCRUD = new PlaceCRUD();
+                $placeCRUD->add($_POST);
+                break;
+            
+            case 'PUT':
+                $_PUT = json_decode(file_get_contents("php://input"), TRUE);
+                $placeCRUD = new PlaceCRUD();
+                $placeCRUD->update($_PUT);
+                break;
+
+            case 'DELETE':
+                $_DELETE = json_decode(file_get_contents("php://input"), TRUE);
+                $placeCRUD = new PlaceCRUD();
+                $placeCRUD->delete($_DELETE);
                 break;
 
             default:
