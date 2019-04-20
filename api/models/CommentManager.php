4108072fa43306240c12abbf1eafbf98470c5e69
Antoine Libert <duncan-arrakis@hotmail.fr>
2019-04-20 18:18:12 +0200

    Add commentManager

---
+++ api/models/CommentManager.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace models;
+
+class CommentManager extends DBAccess {
+
+	public function add(Comment $comment) {
+		$q = $this->db->prepare("INSERT INTO comments 
+      (`content`, `date_created`, `date_edited`, 'id_user', `id_post`) 
+      VALUES (:title, :content, NOW(), NOW(), :id_user, :id_post);");
+      
+		$q->bindValue(':title', $comment->getTitle());
+    $q->bindValue(':content', $comment->getContent());
+    $q->bindValue(':id_user', $comment->getId_user());
+    $q->bindValue(':id_post', $comment->getId_post());
+
+	  $q->execute();
+
+    $comment->hydrate(['id' => $this->db->lastInsertId()]);
+    return $comment;
+  }
+
+  public function count() {
+    return $this->db->query('SELECT COUNT(*) FROM comments;')->fetchColumn();
+  }
+
+  public function readById($id) {
+      $q = $this->db->query('SELECT * FROM comments WHERE id = '.$id);
+      $post = $q->fetch(\PDO::FETCH_ASSOC);
+      return new Comment($comment);
+  }
+
+  public function readByPost($id_post) {
+    $allComment = [];
+    
+    $q = $this->db->query('SELECT * FROM comments 
+      WHERE id_post = '. $id_post .';');
+    while ($data = $q->fetch(\PDO::FETCH_ASSOC)) {
+     $allComments[$data['id']] = new Comment($data);
+    }
+    return $allComments;
+  }
+
+  public function update(Comment $comment) {
+    $q = $this->db->prepare('UPDATE comments SET content = :content, date_edited = NOW(), id_user = :id_post ,id_post = :id_post WHERE id = :id');
+
+    $q->bindValue(':id', $comment->getId());
+    $q->bindValue(':content', $comment->getContent());
+    $q->bindValue(':id_user', $comment->getId_user());
+    $q->bindValue(':id_post', $comment->getId_post());
+
+    $q->execute();
+
+    return $comment;
+  }
+
+
+
