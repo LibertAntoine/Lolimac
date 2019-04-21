ca2f2d2b915c3bd73bf26afea1b46129f81c26e0
Antoine Libert <duncan-arrakis@hotmail.fr>
2019-04-21 09:50:41 +0200

    Create cut URL function in new scan object

---
+++ api/controllers/scans/CutURL.php
@@ -0,0 +1,22 @@
+<?php
+
+	namespace controllers\scans;
+
+
+class CutURL {
+
+    protected $URL_cut;
+
+    public function getURL_cut() {
+        return $this->URL_cut;
+    }
+
+    public function __construct($url) {
+        $url_cutted = explode ("/" , $url);
+        while ($url_cutted[0] != "api") {
+            array_shift($url_cutted);
+        }
+            array_shift($url_cutted);    
+        $this->URL_cut = $url_cutted;
+    }
+}
