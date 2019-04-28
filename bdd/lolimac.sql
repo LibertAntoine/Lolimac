918787e4114d4529491f090c68adccd1667b868f
Nicolas LIENART <nicolas@lienart.net>
2019-04-28 13:12:11 +0200

    Updated DB, including more fields for events, small tweaks

--- bdd/lolimac.sql
+++ bdd/lolimac.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 20 avr. 2019 à 16:52
--- Version du serveur :  5.7.19
--- Version de PHP :  5.6.31
+-- Host: localhost:3306
+-- Generation Time: Apr 28, 2019 at 01:11 PM
+-- Server version: 5.7.25-0ubuntu0.18.04.2
+-- PHP Version: 7.2.15-0ubuntu0.18.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,28 +19,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `lolimac`
+-- Database: `lolimac`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `comments`
+-- Table structure for table `comments`
 --
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
   `content` text NOT NULL,
   `date_created` date NOT NULL,
   `date_edited` date NOT NULL,
   `id_post` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `id_user` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `comments`
+-- Dumping data for table `comments`
 --
 
 INSERT INTO `comments` (`id`, `content`, `date_created`, `date_edited`, `id_post`, `id_user`) VALUES
@@ -51,144 +49,134 @@ INSERT INTO `comments` (`id`, `content`, `date_created`, `date_edited`, `id_post
 -- --------------------------------------------------------
 
 --
--- Structure de la table `events`
+-- Table structure for table `events`
 --
 
-DROP TABLE IF EXISTS `events`;
-CREATE TABLE IF NOT EXISTS `events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
+  `photo_url` text NOT NULL,
   `description` text NOT NULL,
-  `date_start` date NOT NULL,
-  `date_end` date NOT NULL,
-  `date_created` date NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `title` (`title`)
+  `date_start` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `photo_url`, `description`, `date_start`, `date_end`, `date_created`) VALUES
+(1, 'MAIS LOL', 'https://images.unsplash.com/photo-1504564321107-4aa3efddb5bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80', 'c\'est le lol', '2018-12-12 22:22:00', '2018-12-12 00:00:00', '2019-04-27 00:00:00'),
+(2, 'euh coucou', 'https://images.unsplash.com/photo-1504564321107-4aa3efddb5bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80', 'c\'est le lol', '2018-12-11 10:10:10', '2018-12-12 10:10:10', '2019-04-27 23:42:06'),
+(3, 'tÃ©ste dÃ ccÃªnts', 'https://images.unsplash.com/photo-1504564321107-4aa3efddb5bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80', 'c\'est le lol', '2018-12-11 10:10:10', '2018-12-12 10:10:10', '2019-04-27 23:44:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventtypes`
+--
+
+CREATE TABLE `eventtypes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `eventtypes`
+-- Table structure for table `link_events_eventtypes`
 --
 
-DROP TABLE IF EXISTS `eventtypes`;
-CREATE TABLE IF NOT EXISTS `eventtypes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `link_events_eventtypes`
---
-
-DROP TABLE IF EXISTS `link_events_eventtypes`;
-CREATE TABLE IF NOT EXISTS `link_events_eventtypes` (
+CREATE TABLE `link_events_eventtypes` (
   `id_event` int(11) NOT NULL,
-  `id_type` int(11) NOT NULL,
-  PRIMARY KEY (`id_event`,`id_type`)
+  `id_type` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `link_events_users_modules`
+-- Table structure for table `link_events_users_modules`
 --
 
-DROP TABLE IF EXISTS `link_events_users_modules`;
-CREATE TABLE IF NOT EXISTS `link_events_users_modules` (
+CREATE TABLE `link_events_users_modules` (
   `id_event` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `id_module` int(11) NOT NULL,
-  PRIMARY KEY (`id_event`,`id_user`,`id_module`)
+  `id_module` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `link_event_place`
+-- Table structure for table `link_event_place`
 --
 
-DROP TABLE IF EXISTS `link_event_place`;
-CREATE TABLE IF NOT EXISTS `link_event_place` (
+CREATE TABLE `link_event_place` (
   `id_event` int(11) NOT NULL,
-  `id_place` int(11) NOT NULL,
-  PRIMARY KEY (`id_event`,`id_place`)
+  `id_place` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `modules`
+-- Table structure for table `modules`
 --
 
-DROP TABLE IF EXISTS `modules`;
-CREATE TABLE IF NOT EXISTS `modules` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `modules` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `places`
+-- Table structure for table `places`
 --
 
-DROP TABLE IF EXISTS `places`;
-CREATE TABLE IF NOT EXISTS `places` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `places` (
+  `id` int(11) NOT NULL,
   `postcode` int(11) NOT NULL,
   `street` varchar(255) NOT NULL,
   `number` int(11) NOT NULL,
   `city` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `name` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `posts`
+-- Table structure for table `posts`
 --
 
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `date_created` date NOT NULL,
   `date_edited` date NOT NULL,
   `id_event` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `promotions`
---
-
-DROP TABLE IF EXISTS `promotions`;
-CREATE TABLE IF NOT EXISTS `promotions` (
-  `end_year` int(11) NOT NULL,
-  `nickname` varchar(100) NOT NULL,
-  PRIMARY KEY (`end_year`)
+  `id_user` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `promotions`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `promotions` (
+  `end_year` int(11) NOT NULL,
+  `nickname` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `firstname` varchar(22) NOT NULL,
   `lastname` varchar(22) NOT NULL,
   `pseudo` varchar(25) NOT NULL,
@@ -197,10 +185,133 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pwd_hash` varchar(255) NOT NULL,
   `photo_url` varchar(255) NOT NULL,
   `status` int(1) NOT NULL,
-  `year_promotion` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+  `year_promotion` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `pseudo`, `mail`, `phone`, `pwd_hash`, `photo_url`, `status`, `year_promotion`) VALUES
+(21, 'nico', 'lien', 'nicolnt', 'mail@gmail.com', '06 06 06 06 06', '$2y$10$iQs9LjUg./unNub/8xPQv.6zq2UXEdUukvigN3FH..AWCKbBAQQre', 'nom_photo.jpg', 1, 2021);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `title` (`title`);
+
+--
+-- Indexes for table `eventtypes`
+--
+ALTER TABLE `eventtypes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `link_events_eventtypes`
+--
+ALTER TABLE `link_events_eventtypes`
+  ADD PRIMARY KEY (`id_event`,`id_type`);
+
+--
+-- Indexes for table `link_events_users_modules`
+--
+ALTER TABLE `link_events_users_modules`
+  ADD PRIMARY KEY (`id_event`,`id_user`,`id_module`);
+
+--
+-- Indexes for table `link_event_place`
+--
+ALTER TABLE `link_event_place`
+  ADD PRIMARY KEY (`id_event`,`id_place`);
+
+--
+-- Indexes for table `modules`
+--
+ALTER TABLE `modules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `places`
+--
+ALTER TABLE `places`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `promotions`
+--
+ALTER TABLE `promotions`
+  ADD PRIMARY KEY (`end_year`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `eventtypes`
+--
+ALTER TABLE `eventtypes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `modules`
+--
+ALTER TABLE `modules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `places`
+--
+ALTER TABLE `places`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
