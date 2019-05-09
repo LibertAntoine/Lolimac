5f47d676dc40b4c81e0ed28f61c41ebe5ead9b4e
Nicolas LIENART <nicolas@lienart.net>
2019-05-09 12:08:47 +0200

    Fix and refactor

--- api/controllers/Link_events_placesCRUD.php
+++ api/controllers/Link_events_placesCRUD.php
@@ -5,6 +5,7 @@ namespace controllers;
 use \controllers\scans\ScanDataIn;
 use \controllers\scans\TokenAccess;
 use \controllers\scans\TokenCreater;
+use \controllers\PlaceCRUD;
 use \models\Link_events_places;
 use \models\Link_events_placesManager;
 
@@ -22,6 +23,19 @@ class Link_events_placesCRUD {
       throw new \Exception('Lien déjà existant.');
     }
     return TRUE;
+  }
+
+  public function readPlace_ARRAY($dataIn) {
+      $scanDataIn = new ScanDataIn();
+      $scanDataIn->exists($dataIn, ["id_event"]);
+      $data = $scanDataIn->failleXSS($dataIn);
+      $linkManager = new Link_events_placesManager();
+      $link_events_places = $linkManager->readById_event($data["id_event"]);
+      if ($link_events_places) {
+          $placeCRUD = new PlaceCRUD();
+          $place = $placeCRUD->read_OBJ(["id" => $link_events_places->getId_place()]);
+          return $place->toArray();
+      }
   }
 
   public function update($dataIn) {
@@ -50,4 +64,14 @@ class Link_events_placesCRUD {
     return TRUE;
   }
 
+  public function deleteByIdEvent($dataIn) {
+      $scanDataIn = new ScanDataIn();
+      $scanDataIn->exists($dataIn, ["id_event"]);
+      $data = $scanDataIn->failleXSS($dataIn);
+      $token = new TokenAccess();
+      $linkManager = new Link_events_placesManager();
+      $linkManager->deleteByIdEvent($data["id_event"]);
+      return TRUE;
+  }
+  
 }
